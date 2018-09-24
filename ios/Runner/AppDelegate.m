f3d8a401ce82b1c46344623ad97f0b0b24986ce7
Dmitry Sovgyr <dmitry.s@techery.io>
2018-09-24 18:55:52 +0300

    Integrate AppSpector SDK to iOS project

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,18 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+@import AppSpectorSDK;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
+
+  AppSpectorConfig *config = [AppSpectorConfig configWithAPIKey:@"NGVlZjhjOTUtNDU4My00OWE0LTljODctYTk0NzA4M2U1NjU4"];
+  [AppSpector runWithConfig:config];
+
+    // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
